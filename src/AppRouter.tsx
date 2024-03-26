b60f8ebfae6813316315b38235ce433f9a50f26d
unknown <phaolonguyenanhtuan@gmail.com>
2024-03-26 09:49:29 +0700

    add search restaurant and filter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -7,12 +7,22 @@ import AuthCallBackPage from './pages/AuthCallBackPage';
 import UserProfilePage from './pages/UserProfilePage';
 import ProtectedRoute from './auth/ProtectedRoute';
 import ManageRestaurantPage from './pages/ManageRestaurantPage';
+import SearchPage from './pages/SearchPage';
+
 
 const AppRouter = () => {
   return (
     <Routes>
         <Route path='/' element={<Layout showHero><HomePage /></Layout>} />
         <Route path='/auth-callback' element={<AuthCallBackPage />} />
+        <Route
+          path="/search/:city"
+          element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
         <Route element={<ProtectedRoute />}>
             <Route path='/user-profile' element={<Layout><UserProfilePage /></Layout>} />
             <Route path='/manage-restaurant' element={<Layout><ManageRestaurantPage /></Layout>} />
